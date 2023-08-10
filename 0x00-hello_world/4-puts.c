717fa91c0338411cabd8590ce4ef265a6684e1b7
meral412 <meralahmed744@gmail.com>
2023-08-10 14:54:15 -0700

    4 put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
-main()
-{ puts("Programming is like building a multilingual puzzle" \n)};
-return (0);
+int main(void)
+/* this is our first code */
+{
+  puts ( "\"Programming is like building a multilingual puzzle" ); 
+  return(0);
+}
