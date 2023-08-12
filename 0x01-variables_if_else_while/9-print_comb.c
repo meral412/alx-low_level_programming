803ed0a972401ab4bef595138cf860558792880f
meral412 <meralahmed744@gmail.com>
2023-08-12 09:27:01 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+int j;
+i = 0;
+
+while (i < 10)
+{
+j = i + 1;
+while (j < 10)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i != 8 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
