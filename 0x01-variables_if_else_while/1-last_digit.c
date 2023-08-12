3d31503501df5ec7cda0a2051303ff9d25997281
meral412 <meralahmed744@gmail.com>
2023-08-11 17:08:48 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,10 +20,10 @@ n = rand() - RAND_MAX / 2;
 j = n % 10;
 
 if (j > 5)
-printf("last digit of %d is %d and is greater than 5\n", n, j);
+printf("Last digit of %d is %d and is greater than 5\n", n, j);
 if (j == 0)
-printf("last digit of %d is %d and is 0\n", n, j);
+printf("Last digit of %d is %d and is 0\n", n, j);
 if (j < 6 && j != 0)
-printf("last digit of %d is %d and is less than 6 and not 0\n", n, j);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, j);
 return (0);
 }
