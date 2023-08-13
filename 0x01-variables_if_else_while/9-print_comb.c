c7bfd068a7306acc94bafcff102d80383f99dcb3
meral412 <meralahmed744@gmail.com>
2023-08-13 01:33:27 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-int i;
-i = 0;
+int i = 0;
 
 while (i < 10)
 {
-i = i + 1;
-
 putchar(i + '0');
-
-if (i != 8 || i != 9)
+if (i < 9)
 {
 putchar(',');
 putchar(' ');
 }
-}
 i++;
+}
+putchar('\n');
 return (0);
 }
