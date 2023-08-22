9037ab982220691ea61ea48812c4128df4021006
meral412 <meralahmed744@gmail.com>
2023-08-21 17:14:53 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
+void rev_string(char *s);
